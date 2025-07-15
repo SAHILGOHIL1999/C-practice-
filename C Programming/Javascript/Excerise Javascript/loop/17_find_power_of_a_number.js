e14b593ba17a9f917c9c489b1a9a89df12104233
SAHILGOHIL1999 <161831905+SAHILGOHIL1999@users.noreply.github.com>
2025-07-15 10:27:21 +0530

    Update 17_find_power_of_a_number.js

--- C Programming/Javascript/Excerise Javascript/loop/17_find_power_of_a_number.js
+++ C Programming/Javascript/Excerise Javascript/loop/17_find_power_of_a_number.js
@@ -1,1 +1,16 @@
 // Accept a value and find power of a number using for loop.
+
+"use strict";
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let base = parseInt(prompt("Enter the base value: "));
+let exponent = parseInt(prompt("Enter the exponent value: "));
+let result = 1;
+
+for (let i = 1; i <= exponent; i++) {
+    result = result * base;
+}
+
+console.log(`${base} raised to the power of ${exponent} is: ${result}`);
